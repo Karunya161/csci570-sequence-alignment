f8cafbdc15bebcb313947f29f90e36978abfc36f
Karunya161 <karunya.rdg@gmail.com>
2025-11-25 17:26:13 -0800

    Basic Stucture

---
+++ src/basic.py
@@ -0,0 +1,3 @@
+def basic_alignment(X, Y, delta, alpha):
+    # TODO: implement DP version
+    pass
---
+++ src/efficient.py
@@ -0,0 +1,3 @@
+def memory_efficient_alignment(X, Y, delta, alpha):
+    # TODO: implement divide-and-conquer version
+    pass
